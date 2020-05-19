b474ebb1638563d72dc45e1e07308a7e92c23bb8
Manel Abdessalem <manel.abdessalem@epitech.eu>
2020-05-19 17:02:02 +0200

    Creation de theme - En test

---
+++ Manel/wp-content/themes/horrortheme/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <?php wp_head();?>
+</head>
+<body <?php body_class(); ?>>
+    <header class="header">
+        <a href="<?php echo home_url('/');?>">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo.jpeg" alt='Logo'>
+        </a>
+    </header>
+
+    <?php wp_body_open(); ?>
---
+++ Manel/wp-content/themes/horrortheme/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php get_header();?>
+
+        <h1>TEST</h1>
+        
+        <?php get_footer();?>
+    </body>
+</html>
